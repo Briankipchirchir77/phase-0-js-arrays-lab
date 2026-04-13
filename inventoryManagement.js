1b8dcbf4f51010beab34f7e8b7b6b936acc79ff1
Brian <briankipchirchir964@gmail.com>
2026-04-13 17:34:27 +0300

    added some statements

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,32 @@
 // Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}
+// 🔍 Example usage (testing your functions)
+logFirstProduct(); // Laptop
+
+addProduct("Tablet");
+console.log(products);
+
+updateProductName(1, "Smartphone");
+console.log(products);
+
+removeLastProduct();
+console.log(products);
 
 
 // Export the necessary parts for testing
